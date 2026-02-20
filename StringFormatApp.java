bc8147d603e93d806d7dd610a14adae2f5787e55
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-02-20 19:25:56 +0000

    Completed Task 3

---
+++ StringFormatApp.java
@@ -0,0 +1,15 @@
+public class StringFormatApp {
+    public static void main(String[] args) {
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+        float height = Float.parseFloat(args[2]);
+        
+        System.out.printf("name: %-20s Age:%3d Height:%4.2fm%n", name, age, height);
+        
+        float spaceHeight = height * 1.023f * 100;
+        System.out.printf("My space height would be %.1fcm%n", spaceHeight);
+        
+        int martianAge = Math.round(age / 1.88f);
+        System.out.printf("On Mars I would be approximately %d years old.%n", martianAge);
+    }
+}
